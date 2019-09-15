aa676a87dd372a030b7457c98236b7eb548c149f
Martins Obayomi <obayomimartins96@gmail.com>
2019-09-15 02:17:07 +0100

    feature(landing): landing page component [delivers #168496132]

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class LandingPage extends Component {
+  styleColor = {
+    background: "#0c1824",
+    color: "#fff",
+    cursor: "pointer",
+    borderRadius: "4%"
+  }
+  render() {
+    return (
+      <div className="loginCoverPage">
+        <div className="formModal">
+          <Link to="#">
+            <h3>Welcome to Banka</h3>
+          </Link>
+          <Link to='/login'>
+            <h1 style={this.styleColor}>Get started</h1>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default LandingPage;
