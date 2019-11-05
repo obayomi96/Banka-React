37f44307bcc848631961b187625a6fedd8aa2430
Martins Obayomi <obayomimartins96@gmail.com>
2019-11-05 15:32:54 +0100

    User can get accounts and create new account

--- src/components/GetAccounts/GetAccounts.js
+++ src/components/GetAccounts/GetAccounts.js
@@ -6,11 +6,17 @@ import { getAccounts } from '../../actions/accountActions';
 class GetAccounts extends Component {
   state = {
     accounts: [],
-    email: this.props.user.email
+    email: this.props.user.email,
   };
 
   divStyle = {
     textAlign: 'left',
+    background: '#ddd',
+    display: 'grid',
+    lineHeight: '2.5',
+    marginBottom: '20px',
+    borderRadius: '5px',
+    boxShadow: '15px 14px 42px -22px rgba(0,0,0,0.75)',
   }
 
   componentDidMount = async () => {
@@ -28,23 +34,22 @@ class GetAccounts extends Component {
     const userAccounts = accounts.map((acc, index) => {
       if (accounts === []) {
         return (
-          <div>No Accounts Found</div>
+          <div>You have no bank account, kindly create one</div>
         );
       } else {
         return (
           <div key={index} style={this.divStyle}>
             <h4>Account Number: {acc.accountNumber}</h4>
-            <h4>Banlance: {acc.balance}</h4>
+            <h4>Banlance: ${acc.balance}</h4>
             <h4>Account Status: {acc.status}</h4>
-            <h4>Account Type: {acc.type}</h4>
           </div>
         )
       }
     })
     return (
-      <div>
+      <div >
         { loading ? 
-          <PulseLoader  sizeUnit={"px"} size={10} color={'#fff'} />
+          <PulseLoader  sizeUnit={"px"} size={20} color={'#000'} />
           :
           userAccounts
         }
@@ -56,8 +61,8 @@ class GetAccounts extends Component {
 const mapStateToProps = (state) => {
   return {
     user: state.auth.user,
-    account: state.accounts,
-    loading: state.loading,
+    accounts: state.accounts,
+    loading: state.accounts.loading,
   };
 };
 
