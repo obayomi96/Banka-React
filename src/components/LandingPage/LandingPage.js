b67df2d6fd5ad672fdccc377bc02d7e28f97416e
martins obayomi <obayomimartins96@gmail.com>
2020-02-06 00:20:25 +0100

    Landing, Login, signup done

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -7,9 +7,7 @@ const LandingPage = () => {
   return (
     <div className="loginCoverPage">
       <div className="formModal">
-        <Link to="#">
           <h3>Welcome to Banka</h3>
-        </Link>
         <Link to='/login'>
           <h1>Get started</h1>
         </Link>
