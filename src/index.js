f45e6e70f6e9a591b34908ceff0f5600c0ec9ab1
martins obayomi <obayomimartins96@gmail.com>
2020-03-10 11:25:55 +0100

    Remove redundant dependencies

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Offline, Online } from "react-detect-offline";
 import { store, persistor } from './store';
 import App from './App';
 import './App.css';
-
-<div>
-  <Online>Only shown when you're online</Online>
-  <Offline>Only shown offline (surprise!)</Offline>
-</div>
 
 import { confirmToken } from './utils';
 
